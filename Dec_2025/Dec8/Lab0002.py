84d0b812534b01f6c88fc295440d1e6b844c0f48
achmansaxena01 <avachmansaxena@gmail.com>
2025-11-02 12:53:41 +0530

    update5

---
+++ Dec_2025/Dec8/Lab0002.py
@@ -0,0 +1,10 @@
+# Data types in python
+'''
+five types:
+None: An object that does not contains any value
+Numeric: int,float,complex
+Sequences: str , bytes , bytearray ,list , tuples , range
+Sets: Does not allow duplicates
+Maps: used for mapping and reducing
+
+'''
