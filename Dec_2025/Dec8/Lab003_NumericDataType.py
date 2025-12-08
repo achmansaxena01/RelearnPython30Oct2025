1bbbaf1484c0b95ed1926554c1346b282336edbc
achmansaxena01 <avachmansaxena@gmail.com>
2025-12-08 13:41:07 +0530

    update5

---
+++ Dec_2025/Dec8/Lab003_NumericDataType.py
@@ -0,0 +1,22 @@
+a = 10
+b = 2.0
+c = -45
+print(a, b, c)
+print(type(a), type(b), type(c))
+
+d = 10 + 8j # Complex Number
+print(d, type(d))
+
+e = 0B1111  #Binary Number
+print(e, type(e))
+
+f = 0XFF    #Hexadecimal Number
+print(f, type(f))
+
+g = 0o77    #Octal Number
+print(g, type(g))
+
+h = True    #Boolean Variable
+print(h, type(h))
+
+print (9>8)
