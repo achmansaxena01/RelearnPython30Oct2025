6f6c66424e7cc6ae019c6b634a0937d386dd6217
achmansaxena01 <avachmansaxena@gmail.com>
2025-12-29 10:15:58 +0530

    update8

--- Dec_2025/Dec23/Lab005_Strings.py
+++ Dec_2025/Dec23/Lab005_Strings.py
@@ -1,19 +1,20 @@
-s = "Hi !! My name is Achman"
+s = "Hi !! My name is Achman ||*||"
 sd = 'anubhav'
 print(s)
 print(sd)
 sa = '''you are the 
 creator of your destiny'''
+s1 = 'asdfghjk'
 
 print(sa)
-print(s[0])
-print(s*3)
-print(len(s))
+print(s[0]) # to print the first character from starting
+print(s*3)  #to print it 3 times
+print(len(s))  # to print length of string
 
-print(s[0:20])
-print(s[:8])
-print(s[0:])
-print(s[-3:-1])
+print(s[0:20])  #to print the characters between this including the lower limit and (n-1)
+print(s[:8])  #to print from starting till (n-1)th position
+print(s[7:])  #to print from lower limit till end
+print(s1[-4:])
 
 
 
