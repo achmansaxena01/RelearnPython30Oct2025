00f872ee4768fd25a55c6362ea802fcfcb984b62
achmansaxena01 <avachmansaxena@gmail.com>
2025-12-30 13:14:31 +0530

    update8

--- Dec_2025/Dec23/Lab005_Strings.py
+++ Dec_2025/Dec23/Lab005_Strings.py
@@ -14,7 +14,7 @@ print(len(s))  # to print length of string
 print(s[0:20])  #to print the characters between this including the lower limit and (n-1)
 print(s[:8])  #to print from starting till (n-1)th position
 print(s[7:])  #to print from lower limit till end
-print(s1[-4:])
+print(s1[-4:])   #to print from lower limit till end
 
 
 
