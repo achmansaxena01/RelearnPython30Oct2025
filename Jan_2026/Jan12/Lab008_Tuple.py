ced120e19128e46569bdc46d0e7e5dc3effd3695
achmansaxena01 <avachmansaxena@gmail.com>
2026-01-12 17:41:44 +0530

    update10

---
+++ Jan_2026/Jan12/Lab008_Tuple.py
@@ -0,0 +1,14 @@
+''' Tuple :  it can not be modified and it is immutable.
+It also retain the insertion order and allow duplicates.
+It can also hold the different types of data types '''
+
+t1=(1,2,3,4)
+t2=1,2,3,4
+t3 = 9,
+print(t1)
+print(t2)
+print(t3)
+print(t1+t2)
+print(type(t1))
+print(type(t2))
+print(type(t3))
